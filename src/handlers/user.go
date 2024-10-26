3ad8c4e1b1744fc07076662030d21c9ddd2f7542
Tony Chen <a67894521@kimo.com>
2024-10-26 16:52:40 +0800

    API get user balance

--- src/handlers/user.go
+++ src/handlers/user.go
@@ -1,8 +1,37 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/gin-gonic/gin"
+
+	"go-eth/consts"
+)
 
 func GetUserBalance(c *gin.Context) {
-	// TODO: Implement the logic to get the user balance
-	c.JSON(200, gin.H{"balance": "user_balance"})
+	address := common.HexToAddress(c.Param("address"))
+
+	client, err := ethclient.Dial(consts.CHAIN_RPC_URL)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	balance, err := client.BalanceAt(context.Background(), address, nil)
+	fmt.Println(balance.String())
+	var ethBalance big.Float
+	ethBalance.SetString(balance.String())
+	ethBalance.Quo(&ethBalance, big.NewFloat(1e18)).Text('g', 6)
+	fmt.Println(ethBalance.String())
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{"balance": ethBalance.String()})
 }
