b0bf8d824fb356201c19ffc5f831db1390514529
Tony Chen <a67894521@kimo.com>
2024-10-27 17:18:21 +0800

    Using goruntine to sync transaction with multiple user

--- src/cronjob/sync_transaction.go
+++ src/cronjob/sync_transaction.go
@@ -33,47 +33,51 @@ func RunSyncTransaction() {
 
 		chainId := big.NewInt(consts.CHAIN_ID)
 		startTime := time.Now()
+
 		for _, user := range users {
-			log.Printf("Syncing transactions for user: %v", user.Address)
-			log.Printf("Current sync block: %v", user.CurrentSyncBlock)
-			for blockNumber := user.CurrentSyncBlock; blockNumber <= currentBlock; blockNumber++ {
-				block, err := client.BlockByNumber(context.Background(), big.NewInt(int64(blockNumber)))
-				if err != nil {
-					log.Printf("Failed to get block: %v", err)
-				}
-
-				for _, tx := range block.Transactions() {
-					msg, err := types.Sender(types.LatestSignerForChainID(chainId), tx)
+			go func(user *repositories.User) {
+				log.Printf("Syncing transactions for user: %v", user.Address)
+				log.Printf("Current sync block: %v", user.CurrentSyncBlock)
+				for blockNumber := user.CurrentSyncBlock; blockNumber <= currentBlock; blockNumber++ {
+					block, err := client.BlockByNumber(context.Background(), big.NewInt(int64(blockNumber)))
 					if err != nil {
-						log.Printf("Failed to get transaction sender: %v", err)
-						continue
-					}
-					from := msg.Hex()
-					to := tx.To().Hex()
-					if from == user.Address || to == user.Address {
-						log.Printf("Transaction from: %v, to: %v, hash: %v", from, to, tx.Hash().Hex())
+						log.Printf("Failed to get block: %v", err)
 					}
 
-					transaction := &repositories.Transaction{
-						From:     from,
-						To:       to,
-						Hash:     tx.Hash().Hex(),
-						Value:    tx.Value().String(),
-						Gas:      tx.Gas(),
-						GasPrice: tx.GasPrice().String(),
-						Nonce:    tx.Nonce(),
-						Data:     string(tx.Data()),
-					}
-					if _, err := transaction.UpsertOne(); err != nil {
-						log.Printf("Failed to upsert transaction: %v", err)
+					for _, tx := range block.Transactions() {
+						msg, err := types.Sender(types.LatestSignerForChainID(chainId), tx)
+						if err != nil {
+							log.Printf("Failed to get transaction sender: %v", err)
+							continue
+						}
+						from := msg.Hex()
+						to := tx.To().Hex()
+						if from == user.Address || to == user.Address {
+							log.Printf("Transaction from: %v, to: %v, hash: %v", from, to, tx.Hash().Hex())
+						}
+
+						transaction := &repositories.Transaction{
+							From:     from,
+							To:       to,
+							Hash:     tx.Hash().Hex(),
+							Value:    tx.Value().String(),
+							Gas:      tx.Gas(),
+							GasPrice: tx.GasPrice().String(),
+							Nonce:    tx.Nonce(),
+							Data:     string(tx.Data()),
+						}
+						if _, err := transaction.UpsertOne(); err != nil {
+							log.Printf("Failed to upsert transaction: %v", err)
+						}
 					}
 				}
-			}
-			user.CurrentSyncBlock = currentBlock
-			if _, err := user.UpdateBlockNumber(); err != nil {
-				log.Printf("Failed to update user block number: %v", err)
-			}
+				user.CurrentSyncBlock = currentBlock
+				if _, err := user.UpdateBlockNumber(); err != nil {
+					log.Printf("Failed to update user block number: %v", err)
+				}
+			}(user)
 		}
+
 		endTime := time.Now()
 		log.Printf("Block number: %v", currentBlock)
 		log.Printf("Time taken: %v", endTime.Sub(startTime))
